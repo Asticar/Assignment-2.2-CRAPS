b53814d07a9921a4e093a7e576330faa508c3a12
Asticar <147846659+Asticar@users.noreply.github.com>
2025-02-15 01:39:21 -0600

    Add files via upload

--- Module 2 functions.js
+++ Module 2 functions.js
@@ -1,61 +1,23 @@
-function RPS()
+function CRAPS()
 {
-    // A variable that generates a random number between 1 and 3
-    var player1 = Math.ceil(Math.random()*3);
-    // Links the javascript to the HTML file and 
-    document.getElementById("player1").innerHTML = player1;
-    var player2 = Math.ceil(Math.random()*3);
-    document.getElementById("player2").innerHTML = player2;
-// Linking numbers to rock/paper/scissors for player 1
-    if(player1 == 1)
+    // A variable that generates a random number between 1 and 6
+    var die1 = Math.ceil(Math.random()*6);
+    // Links the javascript to the HTML file
+    document.getElementById("die1").innerHTML = die1;
+    var die2 = Math.ceil(Math.random()*6);
+    document.getElementById("die2").innerHTML = die2;
+    var sum = die1 + die2;
+    document.getElementById("sum").innerHTML = sum;
+    if(sum == 7 || sum == 11) // Determines if the sum of die 1 and die 2 would result in craps
     {
-        document.getElementById("player1").innerHTML = "Rock!"; 
+        document.getElementById("results").innerHTML = "CRAPS! You lose!"; 
     }
-    else if (player1 == 2)
+    else if (sum && die1 % 2 == 0) // Determines a win
     {
-        document.getElementById("player1").innerHTML = "Paper!";
+        document.getElementById("results").innerHTML = "You won!";
     }
-    else
+    else // Determines a push
     {
-        document.getElementById("player1").innerHTML = "Scissors!";
+        document.getElementById("results").innerHTML = "You pushed!";
     }
-// Linking numbers to rock/paper/scissors for player 2
-    if(player2 == 1)
-    {
-        document.getElementById("player2").innerHTML = "Rock!"; 
-    }
-    else if (player2 == 2)
-    {
-        document.getElementById("player2").innerHTML = "Paper!";
-    }
-    else
-    {
-        document.getElementById("player2").innerHTML = "Scissors!";
-    }
-// Determining if theres a tie, and if not, who wins
-    if (player1 == player2)
-    {
-        if (player1 == 1)
-            {
-                document.getElementById("results").innerHTML = "Tie! You both had Rock!"; // Shows a tie for rock
-            }
-        else if(player1 == 2)
-            {
-                document.getElementById("results").innerHTML = "Tie! You both had Paper!"; // Shows a tie for paper
-            }
-        else
-            {
-                document.getElementById("results").innerHTML = "Tie! You both had Scissors!"; // Shows a tie for scissors
-            }
-
-    }
-    else if( (player1 == 1 && player2 == 3)  || (player1 == 2 && player2 == 1)  || (player1 == 3 && player2 ==2)) // Determines if player 1 or player 2 wins
-    {
-        document.getElementById("results").innerHTML = "Player 1 wins!"; // Shows player 1 winning
-    }
-    else
-    {
-        document.getElementById("results").innerHTML = "Player 2 wins!"; // Shows player 2 winning
-    }
-    
 }
